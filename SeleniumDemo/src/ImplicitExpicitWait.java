0071098de57e14990a8f942f0a5cb831da12d08d
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-01-29 16:46:47 +0600

    test commit

--- SeleniumDemo/src/ImplicitExpicitWait.java
+++ SeleniumDemo/src/ImplicitExpicitWait.java
@@ -1,8 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ImplicitExpicitWait {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium file\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.trivago.com/");
+		
+		WebElement s = driver.findElement(By.id("querytext"));
+		s.clear();
+		s.sendKeys("Dha");
+		s.sendKeys(Keys.ENTER);
+		
+		driver.findElement(By.cssSelector(".btn.btn--primary.btn--regular.search-button.js-search-button")).click();
+
+		
 
 	}
 
