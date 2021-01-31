374b1b3c8e1d99a1fd0fbfa705c53d6dd7e98a78
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-02-01 00:27:01 +0600

    right click , shift click

--- SeleniumDemo/src/MouseAction.java
+++ SeleniumDemo/src/MouseAction.java
@@ -1,24 +1,32 @@
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class MouseAction {
+public class MouseAction { 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "E:\\Selenium file\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://www.amazon.com/");
+		driver.manage().window().maximize();
 		Actions a= new Actions(driver);
 		/*most specific element
 		 * a.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-accountList']"))).build().perform();
 		 * driver.close();
 		 */
-		WebElement move = driver.findElement(By.xpath("//a[@id='nav-link-accountList']"));
-		a.moveToElement(move).build().perform();
-
+		WebElement move = driver.findElement(By.xpath("//a[@id='nav-link-accountList']")); //mouse hover
+		//a.moveToElement(move).build().perform();  
+		
+		
+		a.moveToElement(driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"))).click().keyDown(Keys.SHIFT).sendKeys("hello").build().perform();
+		a.moveToElement(move).contextClick().build().perform(); // context click is right click
 	}
 
 }
