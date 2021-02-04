958378daeaa626f526425938ab1bba2375592f9a
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-02-04 18:01:16 +0600

    updated

--- SeleniumDemo/src/Calander.java
+++ SeleniumDemo/src/Calander.java
@@ -17,25 +17,21 @@ public class Calander {
 
 		driver.get("https://www.path2usa.com/travel-companions");
 		driver.findElement(By.xpath("//input[@id='travel_date']")).click();
-		
-		
-		while(!driver.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']")).getText().contains("April"))
+
+		while (!driver.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']")).getText()
+				.contains("April")) 
 		{
 			driver.findElement(By.cssSelector("[class=' table-condensed'] [class='next']")).click();
 		}
-		
-		 
-		
+
 		// grab the common attribute and put it in a list and iterate
 		List<WebElement> day = driver.findElements(By.className("day"));
 		int count = driver.findElements(By.className("day")).size();
-		
-		for(int i=0; i< count; i++)
-		{
+
+		for (int i = 0; i < count; i++) {
 			String dayText = driver.findElements(By.className("day")).get(i).getText();
-			
-			if(dayText.equalsIgnoreCase("23"))
-			{
+
+			if (dayText.equalsIgnoreCase("23")) {
 				driver.findElements(By.className("day")).get(i).click();
 				break;
 			}
