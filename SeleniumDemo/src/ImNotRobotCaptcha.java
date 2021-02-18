b3cb97f638ee5dc7cab20ac81b2fc83bc6a6d6fe
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-02-18 14:51:52 +0600

    Error fixed

--- SeleniumDemo/src/ImNotRobotCaptcha.java
+++ SeleniumDemo/src/ImNotRobotCaptcha.java
@@ -1,4 +1,4 @@
- import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -16,22 +16,21 @@ public class ImNotRobotCaptcha {
 
 		driver.get("https://www.google.com/recaptcha/api2/demo");
 		Thread.sleep(6000);
-		
-		int number= findframeID(driver,By.className("recaptcha-checkbox-border"));
-		 driver.switchTo().frame(number);
-		 driver.findElement(By.xpath( " //span[@id='recaptcha-anchor']" )).click();
-		 
-		 Thread.sleep(6000);
-		 
-		//button[contains(text(),'Verify')]"
-		 int number1= findframeID(driver,By.cssSelector("#recaptcha-verify-button"));
-		 driver.switchTo().frame(number1);
-		 Thread.sleep(6000);
-		 driver.findElement(By.cssSelector("#recaptcha-verify-button")).click();
-		 
-		 
-		 
-		 
+
+		int number = findframeID(driver, By.className("recaptcha-checkbox-border"));
+		driver.switchTo().frame(number);
+		driver.findElement(By.xpath(" //span[@id='recaptcha-anchor']")).click();
+		driver.switchTo().defaultContent();
+		Thread.sleep(6000);
+
+		// button[contains(text(),'Verify')]"
+		int number1 = findframeID(driver, By.cssSelector("#recaptcha-verify-button"));
+		driver.switchTo().frame(number1);
+		System.out.println(number1);
+		Thread.sleep(6000);
+		driver.findElement(By.cssSelector("#recaptcha-verify-button")).click();
+		driver.switchTo().defaultContent();
+
 		/*
 		 * 
 		 * System.out.println(driver.findElements(By.tagName("iframe")).size());
@@ -44,20 +43,26 @@ public class ImNotRobotCaptcha {
 
 	}
 
-	public static int findframeID(WebDriver driver, By by)
+	public static int findframeID(WebDriver driver, By by) throws InterruptedException
 
 	{
 		int framecount = driver.findElements(By.tagName("iframe")).size();
+		System.out.println(framecount);
 		int i;
 
 		for (i = 0; i < framecount; i++) {
 			driver.switchTo().frame(i);
+			System.out.println(i);
+			Thread.sleep(6000);
 			int count = driver.findElements(by).size();
 			if (count > 0) {
-				//driver.findElement(by).click();   //span[@id='recaptcha-anchor']
+				// driver.findElement(by).click(); //span[@id='recaptcha-anchor']
 				break;
-			}   else
+			}
+			else {
 				System.out.println("looping continue");
+				driver.switchTo().defaultContent();
+			}
 		}
 
 		driver.switchTo().defaultContent();
