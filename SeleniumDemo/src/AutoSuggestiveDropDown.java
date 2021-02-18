77b52aa929a98afcd325c17c8f517eae30c5023c
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-02-18 17:32:11 +0600

    Handle hidden element with javascript executor

--- SeleniumDemo/src/AutoSuggestiveDropDown.java
+++ SeleniumDemo/src/AutoSuggestiveDropDown.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,6 +22,17 @@ public class AutoSuggestiveDropDown {
 		driver.findElement(By.xpath("//input[@id='fromPlaceName']")).sendKeys(Keys.DOWN);
 		System.out.println(driver.findElement(By.xpath("//input[@id='fromPlaceName']")).getText());
 		//this will not print because this is hidden element selenium cant handle hidden element
+		
+		/*
+		 * JavaScript DOM can extract hidden elements
+		 * Javascript executor use for this
+		 * https://www.w3schools.com/js/js_htmldom.asp 
+		 */
+		
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		String script= "return document.getElementById(\"fromPlaceName\").value;";  //put forward slash so that java can accept it
+		String text=(String) js.executeScript(script);
+		System.out.println(text);
 		
 	}
 
