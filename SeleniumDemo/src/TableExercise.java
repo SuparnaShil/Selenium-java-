3474865100d3f2b233f478be66acdf7c660abead
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-02-22 13:37:38 +0600

    Table exercise

---
+++ SeleniumDemo/src/TableExercise.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TableExercise {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium file\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.cricbuzz.com/live-cricket-scorecard/32238/pak-vs-rsa-3rd-t20i-south-africa-tour-of-pakistan-2021");
+		/*
+		 * URL may changes
+		 */
+		Thread.sleep(6000);
+		//WebElement table=driver.findElement(By.cssSelector("div[class='cb-col cb-col-100 cb-ltst-wgt-hdr']"));
+		WebElement table=driver.findElement(By.cssSelector(".cb-col.cb-col-100.cb-ltst-wgt-hdr"));
+		//System.out.println("1");
+		
+		int rowcount = table.findElements(By.cssSelector(".cb-col.cb-col-100.cb-scrd-itms")).size();
+		
+		//System.out.println(rowcount);
+		Thread.sleep(6000);
+		int count = table.findElements(By.cssSelector(".cb-col.cb-col-100.cb-scrd-itms div:nth-child(3)")).size();
+		
+		//System.out.println(table.findElement(By.cssSelector("div[class='cb-col.cb-col-100.cb-scrd-itms'] div:nth-child(3)")));
+		//int count = table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)")).size();
+		//System.out.println(count);
+		
+		for (int i=0; i<count-2; i++)
+		{
+			System.out.println(table.findElements(By.cssSelector(".cb-col.cb-col-100.cb-scrd-itms div:nth-child(3)")).get(i).getText());
+		}
+		
+		System.out.println(driver.findElement(By.xpath("//div[text()='Extras']/following-sibling::div")).getText());
+		System.out.println(driver.findElement(By.xpath("//div[text()='Total']/following-sibling::div")).getText());
+		
+		
+		
+
+	}
+
+}
