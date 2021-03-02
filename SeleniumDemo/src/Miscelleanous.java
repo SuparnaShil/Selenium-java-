3e1674e717b1619a82ea56a264b5095a34f96437
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-03-02 11:27:55 +0600

    Screenshots taking process

--- SeleniumDemo/src/Miscelleanous.java
+++ SeleniumDemo/src/Miscelleanous.java
@@ -1,9 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.commons.io.FileUtils;
 
 public class Miscelleanous {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver", "E:\\Selenium file\\chromedriver.exe");
@@ -24,7 +30,9 @@ public class Miscelleanous {
 		
 		driver.get("https://google.com");
 		
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);  //casting driver to take screenshots
 		
+		FileUtils.copyFile(src,new File ("C://Users//Suparna Shil//git//Selenium-java-//SeleniumDemo//src//screenshot.png"))  ;          //copy files from the src object to our local machine
 
 	}
 
